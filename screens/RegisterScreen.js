c40dd4e17ff80c171ebbc5817b8d220dac266442
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-05 12:53:14 +0000

    Adding skeleton template for the screens

---
+++ screens/RegisterScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, StyleSheet, TextInput} from 'react-native';
+
+const RegisterScreen = ({props}) => {
+    return (
+        <View>
+            <Text>Register Screen</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default RegisterScreen;
