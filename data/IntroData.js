983c6739953949230aea6bd4998513069ab60a37
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-05 12:28:31 +0000

    Adding data for the introduction and child protection module

---
+++ data/IntroData.js
@@ -0,0 +1,17 @@
+const INTRODUCTION_DATA = [
+    {
+        "id": 1,
+        "title": "Introduction",
+        "description": "Through this policy, Slum Soccer recognizes that",
+        "main_sub_description": "the welfare of the cild is paramount. All children, whatever their age, culture, disability, gender, language, racial origin religious beliefs and/or sexual identity have the right to protection from abuse",
+        "second_sub_description": "All suspicions and allegations of abuse and poor practice wwill be taken seriously and responded to swiftly and appropriately",
+        "third_sub_description": "All staff (paid/unpaid) working in sport have a responsibility to report concerns to the appropriate officer",
+        "canBeModified": false
+    },
+
+    {
+        "id": 2,
+        "title": "Policy Statements",
+        "description": ""
+    }
+]
