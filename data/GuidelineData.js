b79633f7d20328ad21ab4e88957fbe192f564bfc
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-05 19:59:49 +0000

    Adding more data to the JSON files

--- data/GuidelineData.js
+++ data/GuidelineData.js
@@ -16,95 +16,95 @@ const GUIDELINE_DATA = [ // Array of JSON objects to store the guideline data
 
             {
                 "description_id": 1002,
-                "description": "2. Treating all young people/disabled adults equally, and with respect and dignity",
+                "description_practice": "2. Treating all young people/disabled adults equally, and with respect and dignity",
                 "canBeModified": false
             },
 
             {
                 "description_id": 1003,
-                "description": "3. Always putting the welfare of each young person first, before winning or achieving goals",
+                "description_practice": "3. Always putting the welfare of each young person first, before winning or achieving goals",
                 "canBeModified": false
             },
 
             {
                 "description_id": 1004,
-                "description": "4. Maintaining a safe and appropriate distance with players (e.g it's not appropriate for staff or volunteers to have an intimate relationship with a child or to share a room with them).",
+                "description_practice": "4. Maintaining a safe and appropriate distance with players (e.g it's not appropriate for staff or volunteers to have an intimate relationship with a child or to share a room with them).",
                 "canBeModified": false
             },
 
             {
                 "description_id": 1005,
-                "description": "5. Building balanced relationships based on mutual trust with empowers children to share in the decision-making process",
+                "description_practice": "5. Building balanced relationships based on mutual trust with empowers children to share in the decision-making process",
                 "canBeModified": false
             },
 
             {
                 "description_id": 1006,
-                "description": "6. Making sport fun, enjoyable and promoting fair play",
+                "description_practice": "6. Making sport fun, enjoyable and promoting fair play",
                 "canBeModified": false
             },
 
             {
                 "description_id": 1007,
-                "description": "7. Ensuring that if any form of manual/physical support is required, it should be provided openly and according to guidelines provided by the Coach Education Programme. Care is needed, as it is difficult to maintain hand positions when the child is constantly moving. Young people and their parents should always be conslted and their agreement gained",
+                "description_practice": "7. Ensuring that if any form of manual/physical support is required, it should be provided openly and according to guidelines provided by the Coach Education Programme. Care is needed, as it is difficult to maintain hand positions when the child is constantly moving. Young people and their parents should always be conslted and their agreement gained",
                 "canBeModified": false
             },
 
             {
                 "description_id": 1008,
-                "description": "8. Keeping up to date with technical skills, qualifications and insurance in sport.",
+                "description_practice": "8. Keeping up to date with technical skills, qualifications and insurance in sport.",
                 "canBeModified": false
             },
 
             {
                 "description_id": 1009,
-                "description": "9. Involving parents/carers wherever possible. For example, encouraging them to take responsibility for their children in the changing rooms.",
+                "description_practice": "9. Involving parents/carers wherever possible. For example, encouraging them to take responsibility for their children in the changing rooms.",
                 "canBeModified": false
             },
 
             {
                 "description_id": 1010,
-                "description": "10. If groups have to be supervised in the changing rooms, always ensure parents, teachers, coaches or officials work in pairs"
+                "description_practice": "10. If groups have to be supervised in the changing rooms, always ensure parents, teachers, coaches or officials work in pairs"
             },
 
             {
                 "description_id": 1011,
-                "description": "11. Ensuring that if mixed teams are taken away, they should always be accomopanied by a mal and a female member of staff. However, remember that same gender abuse can also occur"
+                "description_practice": "11. Ensuring that if mixed teams are taken away, they should always be accomopanied by a mal and a female member of staff. However, remember that same gender abuse can also occur"
             },
 
             {
                 "description_id": 1012,
-                "description": "12. Ensuring that at tournaments or residential events, adults should not enter in children's rooms or invite children into their rooms"
+                "description_practice": "12. Ensuring that at tournaments or residential events, adults should not enter in children's rooms or invite children into their rooms"
             },
 
             {
                 "description_id": 1013,
-                "description": "13. Being an excellent role model - this includes not smoking or drinking alcohol in the company of young people."
+                "description_practice": "13. Being an excellent role model - this includes not smoking or drinking alcohol in the company of young people."
             },
 
             {
                 "description_id": 1014,
-                "description": "14. Giving enthusiastic and constructive feedback rather than negative criticism."
+                "description_practice": "14. Giving enthusiastic and constructive feedback rather than negative criticism."
             },
 
             {
                 "description_id": 1015,
-                "description": "15. Recognising the developmental needs and capacity of young people and disabled adults - avoiding excessive training or competition and not pushing them against their will."
+                "description_practice": "15. Recognising the developmental needs and capacity of young people and disabled adults - avoiding excessive training or competition and not pushing them against their will."
             },
 
             {
                 "description_id": 1016,
-                "description": "16. Securing parental consent in writing to act in loco parentis, if the needs arise to administer emergency first aid and/or other medical treatment"
+                "description_practice": "16. Securing parental consent in writing to act in loco parentis, if the needs arise to administer emergency first aid and/or other medical treatment"
             },
 
             {
                 "description_id": 1017,
-                "description": "17. Keeping a written record of any injury that occurs, along with the details of any treatment given"
+                "description_practice": "17. Keeping a written record of any injury that occurs, along with the details of any treatment given"
             },
 
             {
                 "description_id": 1018,
-                "description": "Requesting writing parental consent if officials are required to transport young people in their cars."
+                "description_practice": "Requesting writing parental consent if officials are required to transport young people in their cars."
             }
         ],
 
