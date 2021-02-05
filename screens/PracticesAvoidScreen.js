0b727dc56d296639f1171cf3ef5dbd3bced8f1f1
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-05 16:18:50 +0000

    Adding code to store the organisation model data

---
+++ screens/PracticesAvoidScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Virw, Text, Image, FlatList, StyleSheet, ScrollView, SafeAreaView} from 'react-native';
+import {Button} from 'react-native-elements';
+
+const PracticesAvoidScreen = ({props, navigation}) => {
+    return (
+        <View>
+            <FlatList />
+        </View>
+    )
+};
+
+const style = StyleSheet.create({
+
+});
+
+export default PracticesAvoidScreen;
