e4f2c03e964db6187b5926393bfd49b63156460a
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-05 12:31:56 +0000

    Added an organisation model file

--- data/IntroductionData.js
+++ data/IntroductionData.js
@@ -12,6 +12,15 @@ const INTRODUCTION_DATA = [
     {
         "id": 2,
         "title": "Policy Statements",
+        "description": "Slum Soccer has a duty of care to safeguard all children involved in Slum Soccer from harm. All children have a right to protection, and the needs of disabled children and others who may be particularly vulnerable must be taken into account.",
+        "main_sub_description": "Slum Soccer will ensure the safety and protection of all children involved in Slum Soccer through adherence to the Child Protection guidelines adopted by Slum Soccer",
+        "second_sub_description": "A child is defined as a person under the age of 18",
+        "canBeModified": false
+    },
+
+    {
+        "id": 3,
+        "title": "Policy Aims",
         "description": ""
     }
 ]
