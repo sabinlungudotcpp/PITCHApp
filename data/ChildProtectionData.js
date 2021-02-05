66037469cdaff809d36cf981953113591222698b
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-05 19:52:51 +0000

    Adding more data to the JSON files

--- data/ChildProtectionData.js
+++ data/ChildProtectionData.js
@@ -1,13 +1,13 @@
 const CHILD_PROTECTION_DATA = [
     {
         "id": 1,
-        "title": "Promoting good practice",
-        "image": "../assets/Images/ImageGoodPractice.jpg",
-        "description": "Child abuse, particularly sexual abuse, can arouse strong emotions in those facing such a situation. It is important to understand these feelings and not allow them to interfere with your judgement about the appropriate action to take",
-        "main_sub_description": "Abuse can occur within many situations including the home, school and the sporting environment. Some individuals will actively seek employment or voluntary work with young people in order to harm them",
-        "second_sub_description": "A coach, instructor, teacher, official or volunteer will have regular contact with your people and be an important link in identifying cases where they need protection",
-        "third_sub_description": "All suspicious cases of poor practice should be reported following the guidelines in this document",
-        "fourth_sub_description": "When a child enters the organisation having been subjected to child abuse outside the sporting environment, sport can play a crucial role in improving the child's self esteem. In such instances the organizer must work with the appropriate agencies to ensure the child receives the required support",
+        "title_practice": "Promoting good practice",
+        "image_practice": "../assets/Images/ImageGoodPractice.jpg",
+        "description_practice": "Child abuse, particularly sexual abuse, can arouse strong emotions in those facing such a situation. It is important to understand these feelings and not allow them to interfere with your judgement about the appropriate action to take",
+        "main_sub_description_practice": "Abuse can occur within many situations including the home, school and the sporting environment. Some individuals will actively seek employment or voluntary work with young people in order to harm them",
+        "second_sub_description_practice": "A coach, instructor, teacher, official or volunteer will have regular contact with your people and be an important link in identifying cases where they need protection",
+        "third_sub_description_practice": "All suspicious cases of poor practice should be reported following the guidelines in this document",
+        "fourth_sub_description_practice": "When a child enters the organisation having been subjected to child abuse outside the sporting environment, sport can play a crucial role in improving the child's self esteem. In such instances the organizer must work with the appropriate agencies to ensure the child receives the required support",
         "canBeModified": false
     },
 
