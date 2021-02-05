312bf791f8521f1334ea9bd6e1218a97bb90b22c
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-05 21:52:57 +0000

    Adding code to the main screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
 import {View, StyleSheet, Text, SafeAreaView, ImageBackground, FlatList, Image, ScrollView} from 'react-native';
+import {Button} from 'react-native-elements';
 import introData from '../data/IntroductionData';
 
 const HomeScreen = ({props, state, navigation}) => {
     return (
         <ScrollView>
-                <View>
-            <Text style = {style.text}>{introData.map(data => data.module_text)}</Text>
-            <Image style = {style.imageStyle} source = {require('../assets/Images/ImageEnquiries.jpg')} />
+        <View style = {style.mainHeader}>
+            <Text style = {style.mainText}>PITCH</Text>
+        </View>
+
+        <View style = {style.subHeader}>
+            <Text style = {style.subText}>Changing lives for disadvantaged adults </Text>
+        </View>
+
+        <View>
+            <Button style = {style.registerBtn} title = "Register" />
         </View>
     </ScrollView>
         
@@ -16,19 +24,36 @@ const HomeScreen = ({props, state, navigation}) => {
 }
 
 HomeScreen.navigationOptions = {
-    headerTitle: 'Introduction'
+    headerTitle: 'PITCH'
 }
 
 const style = StyleSheet.create({
-    text: {
-        textAlign: 'center',
-        marginVertical: 100
+
+    mainHeader: {
+        alignItems: 'center',
+        flex: 1,
+        marginTop: -60
     },
 
-    imageStyle: {
-        height: 300,
-        width: 300,
-        marginLeft: 50
+    mainText: {
+        marginVertical: 300,
+        marginBottom: 50,
+        fontSize: 35
+    },
+
+    subText: {
+        marginLeft: 40,
+        fontSize: 20,
+        color: 'orange',
+        marginTop: -20
+    },
+
+    registerBtn: {
+        padding: 150,
+        marginTop: -125,
+        paddingLeft: 150,
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 });
 
