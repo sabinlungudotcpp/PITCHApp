d7932cc0d649eaa644e35736d966d217da814a22
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-05 12:56:43 +0000

    Added react native elements API library

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import {View, StyleSheet, TextInput} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import {Button, Input} from 'react-native-elements';
+
+// Author of file: Sabin Constantin Lungu
+// Errors: N/A
+// Date of last modification: 05/02/2021
+// Description: Stores the required code to implement the Register Screen
 
 const RegisterScreen = ({props}) => {
     return (
