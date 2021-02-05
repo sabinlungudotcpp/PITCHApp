930f9774ab0ab9c66bcd7eaacc8f32b940634269
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-05 14:52:15 +0000

    Adding guideline data in JSON format

--- data/GuidelineData.js
+++ data/GuidelineData.js
@@ -3,6 +3,7 @@ const GUIDELINE_DATA = [
         "id": 1001,
         "guideline_id": 1,
         "guideline_title": "Good practice means",
+        "image": "../assets/Images/ImageGuidelines.jpg",
         "guideline_description": "",
         "canBeModified": false
     }
