f80331cb3dbf2a54ef1c49b71ee17bee97eae0a5
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-05 16:10:32 +0000

    Adding code to store the organisation model data

--- models/OrganisationModel.js
+++ models/OrganisationModel.js
@@ -1,1 +1,31 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const NUMBER_OF_BYTES = 10;
+
+const organisationSchema = new mongoose.Schema({ // The organisation schema when registering a new organisation on the portal
+    organisationName: { // The organisation name
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: false
+    },
+
+    mobileNumber: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+
+    country: {
+        type: String,
+        required: true,
+        unique: false
+    }
+});
+
+mongoose.model('Organisation', organisationSchema);
--- server/Server.js
+++ server/Server.js
@@ -1,3 +1,30 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 8040;
+const localhost = '127.0.0.1';
+const okCode = 200;
+const notFound = 404;
+const forbiddenCode = 422;
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Connect to MongoDB server to store the organisation data
+
+app.listen(port, (error) => {
+    if(!error) {
+        return console.log(`Listening for requests on port ${port}`);
+    }
+
+    else {
+        return console.error(error.message);
+    }
+})
