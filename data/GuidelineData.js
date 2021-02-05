260e73cf3ff5edc6d44fd0c95b7c265f015064c0
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-05 20:03:24 +0000

    Adding more data to the JSON files

--- data/GuidelineData.js
+++ data/GuidelineData.js
@@ -1,10 +1,10 @@
 const GUIDELINE_DATA = [ // Array of JSON objects to store the guideline data
     {
         "guideline_id": 1, // Unqiue guideline ID
-        "title": "Good practice guidelines",
-        "sub_heading": "All personnel should be encouraged to demonstrate exemplary behavior in order to promote children's welfare and reduce the likelihood of allegations being made. The following are common sense examples of how to create a positive culture and climate",
-        "image": "../assets/Images/ImageGuidelines.jpg",
-        "guideline_sub_title": "Good practice means:",
+        "title_practice": "Good practice guidelines",
+        "sub_heading_practice": "All personnel should be encouraged to demonstrate exemplary behavior in order to promote children's welfare and reduce the likelihood of allegations being made. The following are common sense examples of how to create a positive culture and climate",
+        "image_practice": "../assets/Images/ImageGuidelines.jpg",
+        "guideline_sub_title_practice": "Good practice means:",
 
         "good_practice_descriptions": [
             {
@@ -109,5 +109,10 @@ const GUIDELINE_DATA = [ // Array of JSON objects to store the guideline data
         ],
 
         "canBeModified": false
+    },
+
+    {
+        "guideline_id": 2,
+        "title_avoid": "Practices to be avoided"
     }
 ];
