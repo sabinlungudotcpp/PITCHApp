8d080b1322c6c288a9d00c978318b696fc57f6a1
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-06 14:32:56 +0000

    Adding code for the Register UI

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -5,8 +5,13 @@ import {Button, Input} from 'react-native-elements';
 const RegisterScreen = () => {
     return (
         <ScrollView>
-                      <Text style = {styles.registerStyle}>Register</Text>
-        <Input placeholder = 'Organisation'/>
+    <Text style = {styles.registerStyle}>Register</Text>
+        <Input placeholder = 'Organisation Name' style = {styles.organisationNameContainer}/>
+        <Input placeholder = 'E-mail' style = {styles.organisationNameContainer}/>
+        <Input placeholder = 'Mobile Number' style = {styles.organisationNameContainer}/>
+        <Input placeholder = 'Country' style = {styles.organisationNameContainer}/>
+        <Input placeholder = 'City' style = {styles.organisationNameContainer}/>
+        <Input placeholder = 'Password' style = {styles.organisationNameContainer}/>
         </ScrollView>
       
     )
@@ -22,6 +27,15 @@ const styles = StyleSheet.create({
         marginTop: 80,
         color: '#F7B500',
         marginLeft: 145
+    },
+
+    organisationNameContainer: {
+        padding: 10,
+        marginTop: 30,
+        width: 50,
+        lineHeight: 20,
+        borderBottomWidth: null,
+
     }
 });
 
