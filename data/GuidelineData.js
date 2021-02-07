a79ec41133ba84cdd8f377feab92ca8981fd8e30
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-07 15:28:12 +0000

    Continuing to add data to the data layer

--- data/GuidelineData.js
+++ data/GuidelineData.js
@@ -118,7 +118,14 @@ const GUIDELINE_DATA = [ // Array of JSON objects to store the guideline data
             {
                 "description_id": 1019,
                 "description_avoid": "1. Avoid spending time alone with children away from others"
+            },
+
+            {
+                "description_id": 1020,
+                "descripton_avoid": "2. Avoid taking or dropping off a child to an event or activity"
             }
-        ]
+        ],
+
+        "canBeModified": false
     }
 ];
