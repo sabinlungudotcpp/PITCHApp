44ecd8778ab67c18d0ca89acda889f05319ac02e
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-09 18:06:11 +0000

    Re-styling register page

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import {View, StyleSheet, ScrollView, Text} from 'react-native';
-import {Button, Input} from 'react-native-elements';
+import {TextInput} from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import {Button} from 'react-native-elements';
+
 
 const RegisterScreen = ({navigation}) => {
     return (
-        <ScrollView>
+<ScrollView>
     <Text style = {styles.registerStyle}>Register</Text>
-        <Input leftIcon = {{name: 'home', paddingTop: 20, color: '#F7B500', marginLeft: 18, marginRight: -1}} placeholder = 'Organisation Name' style = {styles.organisationNameContainer}/>
-        <Input leftIcon = {{name: 'email', paddingTop: -1, color: '#F7B500', marginLeft: 18, marginRight: 10}} placeholder = 'E-mail' style = {styles.emailContainer}/>
-        <Input leftIcon = {{name: 'phone', paddingTop: 1, paddingLeft: 8, marginRight: 10, color: '#F7B500', marginLeft: 10}} placeholder = 'Mobile Number' style = {styles.mobileNumberContainer}/>
-        <Input leftIcon = {{name: 'cloud', paddingTop: 1, marginLeft: 20, marginRight: 10,  color: '#F7B500'}} placeholder = 'Country' style = {styles.countryContainer}/>
-        <Input leftIcon = {{name: 'image', color: '#F7B500', marginLeft: 18, marginRight: 11}} placeholder = 'City' style = {styles.cityContainer}/>
-        <Input leftIcon = {{name: 'lock', color :'#F7B500', marginLeft: 17, marginRight: 10}} placeholder = 'Password' style = {styles.passwordContainer}/>
+    <Text style = {styles.organisationTextHeader}>Organisation Name</Text>
+
+    <TextInput placeholder = "Organisation Name" style = {styles.organisationNameContainer}/>
+    <Text style = {styles.emailTextContainer}>E-mail Address</Text>
+
+    <TextInput placeholder = "E-mail Address"  style = {styles.emailContainer}/>
+    <TextInput placeholder = 'Mobile Number' style = {styles.mobileNumberContainer}/>
+    <TextInput placeholder = 'Country' style = {styles.countryContainer}/>
+
+        <TextInput style = {styles.cityContainer}/>
+        <TextInput style = {styles.passwordContainer}/>
 
     <View>
         <TouchableOpacity onPress = {() => navigation.navigate('Login')}>
@@ -28,46 +35,96 @@ const RegisterScreen = ({navigation}) => {
     )
 };
 
-RegisterScreen.navigationOptions = {
-    headerTitle: 'Back'
-}
-
 const styles = StyleSheet.create({
     registerStyle: {
         fontSize: 30,
-        marginTop: 45,
+        marginTop: 25,
         color: 'black',
         marginLeft: 145
     },
 
-    organisationNameContainer: {
-        padding: 10,
-        marginTop: 30,
+    organisationTextHeader: {
+        marginLeft: 123,
+        top: 5,
+        marginTop: 40,
+        fontSize: 20,
+        marginBottom: -15,
+        color: 'black'
+    },
+
+    emailTextContainer: {
+        marginLeft: 137,
+        top: 5,
+        marginTop: 50,
+        fontSize: 20,
         marginBottom: 1,
-        width: 50,
-        lineHeight: 20,
-        borderBottomWidth: null,
-        textTransform: 'uppercase',
-        height: 10,
-        margin: 10,
-        left: -10,
-        color: 'black',
+    },
+
+    organisationNameContainer: {
+        borderColor: 'black',
+        width: 230,
+        borderRadius: 200,
+        marginTop: 40,
+        borderColor: '#F7B500',
+        marginLeft: 100,
+        borderWidth: 2,
+        padding: 10,
+        textAlign: 'center',
+        fontSize: 15,
+        marginBottom: -30
     },
 
     emailContainer: {
-        padding: 1,
-        width: 50,
-        borderBottomWidth: null,
-        height: 10,
-        left: 1
+        borderColor: 'black',
+        width: 230,
+        borderRadius: 200,
+        marginTop: 20,
+        borderColor: '#F7B500',
+        marginLeft: 100,
+        borderWidth: 2,
+        padding: 10,
+        textAlign: 'center'
     },
 
     mobileNumberContainer: {
-        left: 1
+        borderColor: 'black',
+        width: 225,
+        borderRadius: 200,
+        marginTop: 38,
+        borderColor: 'orange',
+        marginLeft: 100,
+        borderWidth: 2,
+        padding: 10,
+        textAlign: 'center',
+        fontSize: 15,
     },
 
     countryContainer: {
-        left: 1
+        borderColor: 'black',
+        width: 215,
+        borderRadius: 200,
+        marginTop: 40,
+        borderColor: '#F7B500',
+        marginLeft: 100,
+        borderWidth: 3,
+        padding: 10,
+        textAlign: 'center',
+        fontSize: 15,
+        marginBottom: -30
+    },
+
+    passwordContainer: {
+        borderColor: 'black',
+        width: 215,
+        borderRadius: 200,
+        marginTop: 40,
+        borderColor: '#F7B500',
+        marginLeft: 100,
+        borderWidth: 3,
+        padding: 10,
+        textAlign: 'center',
+        fontSize: 15,
+        marginBottom: 30
     },
 
     alreadyText: {
@@ -77,7 +134,7 @@ const styles = StyleSheet.create({
     },
 
     registerBtn: {
-        width: 120,
+        width: 150,
         marginLeft: 140,
         marginTop: 28
     },
