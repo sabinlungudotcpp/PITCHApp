93e0b94cf6e212349bc9553b5dd2b800ca20608e
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-13 10:23:06 +0000

    Adding code to the modules screen

--- navigation/SlumSoccerStackNavigator.js
+++ navigation/SlumSoccerStackNavigator.js
@@ -9,7 +9,8 @@ const Stack = createStackNavigator(); // Creates a stack navigator
 const SlumSoccerStackNavigator = ({navigation}) => {
     return <Stack.Navigator>
         <Stack.Screen options = { {headerTintColor: 'black', headerLeft: () => (<Button onPress={() => navigation.navigate('ModulesScreen')} title = "Modules"
-              color="#fff"/>) , headerTitle: 'Slum Soccer', headerStyle: {backgroundColor: '#F7B500'}}} name = "Back" component = {SlumSoccerHome}/>
+              color="#fff"/>), headerRight: () => (<Button onPress={() => navigation.navigate('Register')} title = "Register"
+              color="#fff"/>), headerTitle: 'Slum Soccer', headerStyle: {backgroundColor: '#F7B500'}}} name = "Back" component = {SlumSoccerHome}/>
 
 <Stack.Screen options = { {headerTintColor: 'black', headerLeft: () => (<Button onPress={() => navigation.navigate('ModulesScreen')} title = "Modules"
               color="#fff"/>) , headerTitle: 'Slum Soccer', headerStyle: {backgroundColor: '#F7B500'}}} name = "ChildProtectionScreen" component = {ChildProtectionScreen}/>
--- screens/AboutUsScreen.js
+++ screens/AboutUsScreen.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {View, Text, StyleSheet, ScrollView, FlatList, Image} from 'react-native';
 import {Button} from 'react-native-elements';
 import introductionData from '../data/IntroductionData';
+import Card from '../components/Card';
 
 const AboutUsScreen = ({item, navigation}) => {
     return (
 
     <ScrollView>
         <FlatList data = {introductionData} renderItem = {({item}) => <Text style = {style.aboutTitle}>{item.about_title}</Text>} keyExtractor = {(item) => item.id}/>
-        <Image style = {{width: 310, height: 185, marginLeft: 50}} source = {require('../assets/Images/SlumSoccer.jpg')} />
+        <Image style = {{width: 350, height: 185, marginLeft: 38}} source = {require('../assets/Images/SlumSoccer.jpg')} />
 
         <FlatList data = {introductionData} renderItem = {({item}) => <Text style = {style.about_description}>{item.about_description}</Text>} keyExtractor = {(item) => item.id}/>
         <FlatList data = {introductionData} renderItem = {({item}) => <Text style = {style.about_sub}>{item.about_sub_description}</Text>} keyExtractor = {(item) => item.id}/>
@@ -20,17 +21,17 @@ const AboutUsScreen = ({item, navigation}) => {
 const style = StyleSheet.create({
 
     aboutTitle: {
-        marginTop: 65,
+        marginTop: 60,
         marginBottom: -55,
         textAlign: 'center',
-        fontSize: 32,
+        fontSize: 34,
         color: '#F7B500',
         right: 2
     },
 
     about_description: { // The description of the About Us
         padding: 15,
-        marginVertical: 30,
+        marginVertical: 33,
         fontSize: 18,
         marginLeft: 20,
         marginBottom: -59
