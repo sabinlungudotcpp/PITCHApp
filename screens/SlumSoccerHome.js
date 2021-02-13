9a7760ab3ad3365b5e9e0ef8490197c2998538f6
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-13 09:45:01 +0000

    Adding code to the modules screen

--- screens/SlumSoccerHome.js
+++ screens/SlumSoccerHome.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import {View, Text, Image, StyleSheet, FlatList, ScrollView} from 'react-native';
 import {Button} from 'react-native-elements';
-import Card from '../components/Card';
-import ModuleData from '../data/ModuleData';
 
 const SlumSoccerHome = ({props, navigation}) => {
     return (
         <View>
-            <Text>HI</Text>
+            <Text style = {styles.introTxt}>Slum Soccer Home Screen</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    introTxt: {
+        
+    }
+});
 
 export default SlumSoccerHome;
