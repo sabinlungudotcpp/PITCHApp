b234b3947d4509c09d0192af9f29da71062dce2a
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-15 22:38:34 +0000

    Finishing off the guideline data

--- data/GuidelineData.js
+++ data/GuidelineData.js
@@ -33,6 +33,23 @@ const GUIDELINE_DATA = [ // Array of JSON objects to store the guideline data
         "guideline_id": 6,
         "title_training": "Training",
         "training_description": "In addition to pre-selection checks, the safeguarding process includes training after recruitment to help staff and volunteers"
+    },
+
+    {
+        "guideline_id": 7,
+        "title_allegations": "Responding to allegations",
+        "allegations_description": "It is not the responsibility of anyone working in Slum Soccer, in a paid or unpaid capacity to decide whether or not child abuse has taken place. However, there is a responsibility to act on any concerns by reporting these to the appropriate officer or the appropriate authorities.",
+        "allegations_sub": "Slum Soccer will assure all staff/volunteers that it will fully support and protect anyone, who in good faith reports his or her concern that a colleague is, or may be, abusing a child. Where there is a complaint against a member of staff there may be three types of investigation",
+        "allegations_first": "1. A criminal investigation",
+        "allegations_second": "2. A child protection investigation",
+        "allegations_third": "3. A disciplinary or misconduct investigation",
+        "allegations_description": "The results of the police and child protection investigation may well influence and inform the disciplinary investigation, but all available information will be used to reach a decision."
+    },
+
+    {
+        "guideline_id": 8,
+        "title_concerns": "Reporting concerns",
+        "concerns_description": "If, following consideration, the allegtaion is clearly about poor practice the designated Child Protection Officer will deal with it as a misconduct issue."
     }
     
 ]
