1f94560fcf8a42f1d33ffb1f868b9fd02ab60031
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-17 16:15:33 +0000

    Adding code to the Child Protection Intro screen

--- screens/IntroductionScreen.js
+++ screens/IntroductionScreen.js
@@ -22,11 +22,11 @@ const IntroductionScreen = ({props, navigation, item}) => {
         <FlatList data = {introductionData} renderItem = {({item}) => <Text style = {styles.introText}>{item.main_sub_description_intro}</Text>} keyExtractor = {(item) => item.id} />
      </Card>
 
-        <View>
-
+        <View style = {styles.btnContainers}>
+             <Button buttonStyle = {{backgroundColor: '#F7B500', theme: 'dark', borderRadius: 200}} style = {styles.backBtn} title = "Back"/>
+             <Button buttonStyle = {{backgroundColor: '#F7B500', theme: 'dark', borderRadius: 200}} style = {styles.aimsBtn} title = "View Aims"/>
         </View>
-     <Button title = "Back"/>
-     <Button title = "View Aims"/>
+     
 </ScrollView>
 
     )
@@ -50,10 +50,43 @@ const styles = StyleSheet.create({
         paddingLeft: -20,
         marginBottom: 10,
         width: 350,
-        height: 170,
+        height: 160,
         marginLeft: 40,
         alignItems: 'center',
         maxWidth: '100%'
+    },
+
+    btnContainers: {
+        width: 130, // The width of the button
+        height: 42,
+        textAlign: 'center',
+        borderRadius: 200,
+        marginLeft: 45,
+        marginTop: 10,
+        paddingHorizontal: -15,
+        justifyContent: 'space-between',
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingRight: 1,
+    },
+
+    backBtn: {
+        width: 160,
+         right: -5,
+         left: -15,
+         marginLeft: 5,
+         color: 'black',
+         textAlign: 'center'
+    },
+
+    aimsBtn: {
+        width: 155,
+         right: -5,
+         left: -10,
+         marginLeft: 20,
+         color: 'black',
+         textAlign: 'center'
     }
 });
 
