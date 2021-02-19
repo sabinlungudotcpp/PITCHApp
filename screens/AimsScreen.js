34d6ad2d986ed2c1a8efc1dc27eff4c22b2616a4
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-19 12:34:50 +0000

    Adding code to the aims screen

--- screens/AimsScreen.js
+++ screens/AimsScreen.js
@@ -5,16 +5,24 @@ import { FlatList, ScrollView } from 'react-native-gesture-handler';
 import Card from '../components/Card';
 import introductionData from '../data/IntroductionData';
 
-const AimsScreen = ({props, navigation, item}) => {
+const AimsScreen = ({props, navigation, item}) => { // Aims Screen Component
     return (
         <ScrollView>
             <Text style = {styles.aimsText}>{introductionData.map((value) => value.title_statement)}</Text>
 
-            <Card style = {styles.cardStyle}>
+        <Card style = {styles.cardStyle}>
             <FlatList data = {introductionData} keyExtractor = {(item) => item.id} renderItem = {({item}) => <Text style = {styles.statementTxt}>{item.description_statement}</Text>} />
+        </Card>
 
-            </Card>
-        </ScrollView>
+        <Text style = {styles.aimsContainer}>{introductionData.map(value => value.title_aims)}</Text>
+
+        <Card style = {styles.cardStyle}>
+
+        </Card>
+
+        <Button title = "Back" />
+        <Button title = "View Policies" />
+    </ScrollView>
     )
 };
 
@@ -22,19 +30,25 @@ const styles = StyleSheet.create({
     aimsText: {
         textAlign: 'center',
         fontSize: 32,
-        marginTop: 100,
+        marginTop: 60,
+        color: '#F7B500'
+    },
+
+    aimsContainer: {
+        textAlign: 'center',
+        fontSize: 32,
         color: '#F7B500'
     },
 
     cardStyle: {
-        padding: 5,
+        padding: 4,
         paddingLeft: -40,
         paddingBottom: -5,
         marginBottom: 40,
         width: 350,
         height: 160,
         marginLeft: 35,
-        marginTop: 40,
+        marginTop: 35,
         alignItems: 'center',
         maxWidth: '100%'
     },
