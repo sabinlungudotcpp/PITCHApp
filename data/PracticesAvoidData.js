0ed4a90cf700b0965a1ed5e1c79dd5b87e488df1
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-24 13:05:09 +0000

    Adding code for the Practices to avoid / sanction screen

---
+++ data/PracticesAvoidData.js
@@ -0,0 +1,7 @@
+const PRACTICES_AVOID_DATA = [
+    {
+        "id": 1,
+        "description": "The following should be avoided except in emergenceis. If cases arise where these situations are unavoidable it should be with the full knowledge and consent of someone in charge in the organization or the child's parents.",
+        "avoid_list": "1. Avoid spending time alone with children away from others."
+    }
+]
--- screens/PracticesAvoidScreen.js
+++ screens/PracticesAvoidScreen.js
@@ -6,9 +6,12 @@ import Card from '../components/Card'; // Imports the Card component
 const PracticesAvoidScreen = ({props, navigation}) => { // The Practices to Avoid Screen
     return (
         <ScrollView>
-            <View style = {style.mainView}>
-            <FlatList />
-         </View>
+            <Text style = {style.practicesAvoidTxt}>Practices to Avoid</Text>
+
+            <Card style = {style.cardStyle}>
+                
+            </Card>
+
         </ScrollView>
     )
 };
@@ -16,6 +19,26 @@ const PracticesAvoidScreen = ({props, navigation}) => { // The Practices to Avoi
 const style = StyleSheet.create({
     mainView: {
         flex: 1
+    },
+
+    practicesAvoidTxt: {
+        textAlign: 'center',
+        marginTop: 60,
+        fontSize: 31,
+        color: '#F7B500'
+    },
+
+    cardStyle: { // The style for the card
+        padding: 8, // Padding of 8 pixels
+        paddingLeft: -40,
+        paddingBottom: -5,
+        marginBottom: 30,
+        width: 350,
+        height: 130,
+        marginLeft: 35,
+        marginTop: 35,
+        alignItems: 'center',
+        maxWidth: '100%'
     }
 });
 
