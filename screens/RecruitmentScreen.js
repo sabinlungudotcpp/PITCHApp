c8f9aa5bc16e386ea0501c36b769aa288a2fbbe2
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-24 12:38:20 +0000

    Adding code for the Recruitment Screen

---
+++ screens/RecruitmentScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, StyleSheet, Text, ScrollView, FlatList} from 'react-native';
+import {Button} from 'react-native';
+
+const RecruitmentScreen = ({props, navigation, item}) => {
+
+};
+
+const style = StyleSheet.create({ // The styles for the recruitment screen
+
+});
+
+export default RecruitmentScreen;
