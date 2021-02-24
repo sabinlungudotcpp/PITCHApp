b44dd24184d0b54c06e7409de49e5d678d7bdb7d
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-24 15:20:02 +0000

    Adding code for the Practices avoid and saction data

---
+++ screens/guidelines/RecruitmentGuidelines.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, ScrollView, StyleSheet, FlatList} from 'react-native';
+import {Button} from 'react-native-elements';
+import Card from '../../components/Card';
+
+const RecruitmentGuidelines = ({props, navigation, items}) => {
+    return (
+        <ScrollView>
+            <Text>Recruitment Guidelines</Text>
+        </ScrollView>
+    )
+};
+
+export default RecruitmentGuidelines;
