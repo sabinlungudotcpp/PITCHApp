f72aeda7821159de508e1ad8cbee1c49c0fb2b70
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-26 18:44:47 +0000

    Adding Data to the child protection module

--- screens/AllegationsScreen.js
+++ screens/AllegationsScreen.js
@@ -5,10 +5,28 @@ import Colors from '../constants/Colors';
 import Card from '../components/Card';
 import ChildProtectionData from '../data/ChildProtectionData';
 
+const MARGIN_SIZES= {
+    marginTop: 35,
+    marginBottom: 30
+}
+
+const PADDING_SIZES = { // Padding sizes
+
+}
+
 const AllegationsScreen = ({item, navigation, props}) => {
     return (
         <ScrollView>
             <Text style = {style.allegationMainTxt}>{ChildProtectionData.map(value => value.allegation_title)}</Text>
+
+            <Card style = {style.cardStyle}>
+
+            </Card>
+
+            <Card style = {style.cardStyle}>
+                
+            </Card>
+            
         </ScrollView>
     )
 };
@@ -22,7 +40,16 @@ const style = StyleSheet.create({
     },
 
     cardStyle: {
-        
+        padding: 10, // Has a padding space of 12
+        paddingLeft: -40, // Padding left of -40
+        paddingBottom: -5,
+        marginBottom: MARGIN_SIZES.marginBottom,
+        width: 340,
+        height: 150, // The height of the card is 150 pixels
+        marginLeft: 45,
+        marginTop: 35,
+        alignItems: 'center', // Aligns the content of the items to the center
+        maxWidth: '100%' // Maximum width of 100%
     }
 });
 
