b6da07ccb712fb09623a5055b544585a8bd12a3a
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-26 17:27:53 +0000

    Adding Data to the child protection module

---
+++ screens/AllegationsScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, StyleSheet, ScrollView, FlatList, Text} from 'react-native';
+import {Button} from 'react-native';
+import Colors from '../constants/Colors';
+import Card from '../components/Card';
+
+const AllegationsScreen = ({item, navigation, props}) => {
+
+};
+
+export default AllegationsScreen; // Exports the allegations screen
