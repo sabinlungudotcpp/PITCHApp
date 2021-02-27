9e15997b4e2b6f6c7ae250f78dd9696aea877852
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-27 20:07:22 +0000

    Adding buttons to the allegations screen

--- screens/PoorPracticeScreen.js
+++ screens/PoorPracticeScreen.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import {View, ScrollView, Text, FlatList} from 'react-native';
+import {View, ScrollView, Text, FlatList, StyleSheet} from 'react-native';
 import ChildProtectionData from '../data/ChildProtectionData';
 import Card from '../components/Card';
 
 const PoorPracticeScreen = ({item, navigation, props}) => {
     return (
         <ScrollView>
-            <Text>Poor Practice Screen</Text>
+            <Text style = {styles.policyTitleTxt}>{ChildProtectionData.map(value => value.poor_practice_title)}</Text>
         </ScrollView>
     )
 };
+
+const styles = StyleSheet.create({
+    policyTitleTxt: {
+        textAlign: 'center'
+    }
+});
+
 
 export default PoorPracticeScreen;
