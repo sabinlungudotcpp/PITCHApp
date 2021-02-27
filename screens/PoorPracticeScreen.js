3ddc62bb128e7ef785d0229e981688c5322cee9c
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-02-27 20:19:30 +0000

    Adding code to the reporting poor practice concerns screen

--- screens/PoorPracticeScreen.js
+++ screens/PoorPracticeScreen.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import {View, ScrollView, Text, FlatList, StyleSheet} from 'react-native';
+import {View, ScrollView, Text, FlatList, StyleSheet, Image} from 'react-native';
 import ChildProtectionData from '../data/ChildProtectionData';
 import Card from '../components/Card';
+import Colors from '../constants/Colors';
 
 const PoorPracticeScreen = ({item, navigation, props}) => {
     return (
         <ScrollView>
             <Text style = {styles.policyTitleTxt}>{ChildProtectionData.map(value => value.poor_practice_title)}</Text>
+            <Image style = {styles.concernImg} source = {(require('../assets/Images/ImageConcerns.jpg'))} />
         </ScrollView>
     )
 };
 
 const styles = StyleSheet.create({
     policyTitleTxt: {
-        textAlign: 'center'
+        textAlign: 'center',
+        marginTop: 50,
+        color: Colors.primaryColor,
+        fontSize: 29
+    },
+
+    concernImg: {
+        width: 300,
+        height: 160,
+        marginLeft: 60,
+        marginTop: 30
     }
 });
 
 
-export default PoorPracticeScreen;
+export default PoorPracticeScreen; // Export the poor practice screen
