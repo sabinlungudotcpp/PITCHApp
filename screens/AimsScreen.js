dc8e78c033c1950c65287fdf6ecd8f917006ca2a
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-03-03 19:23:16 +0000

    Adding code to make the UI look consistent on Android

--- screens/AimsScreen.js
+++ screens/AimsScreen.js
@@ -11,28 +11,37 @@ const SIZES = {
 }
 
 const AimsScreen = ({props, navigation}) => { // Aims Screen Component
-    return (
-        <ScrollView>
-            <Text style = {styles.aimsText}>{introductionData.map((value) => value.title_statement)}</Text>
-
-        <Card style = {styles.cardStyle}>
-            <FlatList data = {introductionData} keyExtractor = {(item) => item.id} renderItem = {({item}) => <Text style = {styles.statementTxt}>{item.description_statement}</Text>} />
-        </Card>
-
-        <Text style = {styles.aimsContainer}>{introductionData.map(value => value.title_aims)}</Text>
-
-        <Card style = {styles.cardStyle}>
-            <FlatList data = {introductionData} keyExtractor = {(item) => item.id} renderItem = {({item}) => <Text style = {styles.aimsTextContainer}>{item.description_aims}</Text>}/>
-            <FlatList data = {introductionData} keyExtractor = {(item) => item.id} renderItem = {({item}) => <Text style = {styles.aimsTextContainer}>{item.main_sub_description_aims}</Text>} />
-        </Card>
-
-        <View style = {styles.btnContainers}>
-            <Button style = {styles.backBtn} buttonStyle = {{backgroundColor: Colors.primaryColor, borderRadius: SIZES.borderRadius}} title = "Back" onPress = {() => navigation.navigate('IntroductionScreen')} />
-            <Button style = {styles.policyBtn} buttonStyle = {{backgroundColor: Colors.primaryColor, borderRadius: SIZES.borderRadius}} title = "View Policies" onPress = {() => navigation.navigate('GoodPracticeScreen')} />
-        </View>
+    try {
+        return (
+            <ScrollView>
+                <Text style = {styles.aimsText}>{introductionData.map((value) => value.title_statement)}</Text>
+    
+            <Card style = {styles.cardStyle}>
+                <FlatList data = {introductionData} keyExtractor = {(item) => item.id} renderItem = {({item}) => <Text style = {styles.statementTxt}>{item.description_statement}</Text>} />
+            </Card>
+    
+            <Text style = {styles.aimsContainer}>{introductionData.map(value => value.title_aims)}</Text>
+    
+            <Card style = {styles.cardStyle}>
+                <FlatList data = {introductionData} keyExtractor = {(item) => item.id} renderItem = {({item}) => <Text style = {styles.aimsTextContainer}>{item.description_aims}</Text>}/>
+                <FlatList data = {introductionData} keyExtractor = {(item) => item.id} renderItem = {({item}) => <Text style = {styles.aimsTextContainer}>{item.main_sub_description_aims}</Text>} />
+            </Card>
+    
+            <View style = {styles.btnContainers}>
+                <Button style = {styles.backBtn} buttonStyle = {{width: Platform.OS === 'android' ? 130 : null, backgroundColor: Colors.primaryColor, borderRadius: SIZES.borderRadius}} title = "Back" onPress = {() => navigation.navigate('IntroductionScreen')} />
+                <Button style = {styles.policyBtn} buttonStyle = {{width: Platform.OS === 'android' ? 130 : null, backgroundColor: Colors.primaryColor, borderRadius: SIZES.borderRadius}} title = "View Policies" onPress = {() => navigation.navigate('GoodPracticeScreen')} />
+            </View>
+        </ScrollView>
         
-    </ScrollView>
-    )
+        )
+    } 
+    
+    catch(error) {
+
+        if(error) {
+            return console.error(`Cause of error ${error.toString()} - recorded at ${Date.now()}`);
+        }
+    }
 };
 
 const styles = StyleSheet.create({
