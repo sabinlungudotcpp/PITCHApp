750572ec26cc8a00e5765715329a7d8d93fff399
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-03-10 08:20:52 +0000

    Adding data to the Bullying Screen

--- screens/BullyingScreen.js
+++ screens/BullyingScreen.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import {ScrollView, Text, FlatList, Platform, StyleSheet, Image} from 'react-native';
+import {ScrollView, Text, FlatList, Platform, StyleSheet, Image, SafeAreaView, View} from 'react-native';
 import {Button} from 'react-native-elements';
 import Colors from '../constants/Colors';
 import Card from '../components/Card';
 import ChildProtectionData from '../data/ChildProtectionData';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const BullyingScreen = ({item, navigation}) => {
     return (
-        <ScrollView>
-            <Text style = {styles.bullyingTxt}>{ChildProtectionData.map(title => title.title_bullying)}</Text>
+    <SafeAreaView style = {styles.safeView}>
 
+            <ScrollView>
+            <Text style = {styles.bullyingTxt}>{ChildProtectionData.map(title => title.title_bullying)}</Text>
             <Image style = {styles.bullyingImg} source = {(require('../assets/Images/ImageBullying.jpg'))} />
+
+            <Card style = {styles.cardStyles}>
+                        
+            </Card>
+
+            <View style = {styles.btnContainer}>
+                <Button buttonStyle = {{backgroundColor: Colors.primaryColor, borderRadius: 200}} style = {styles.backBtn} title = "Back" />
+
+                <TouchableOpacity onPress = {() => {}}>
+                     <Button buttonStyle = {{backgroundColor: Colors.primaryColor, borderRadius: 200}} style = {styles.backBtn} title = "Next Policy" />
+                </TouchableOpacity>
+            </View>
+
         </ScrollView>
+    </SafeAreaView>
+
+        
     )
 };
 
@@ -21,6 +39,23 @@ const styles = StyleSheet.create({
         color: Colors.primaryColor,
         textAlign: 'center',
         marginTop: 30
+    },
+
+    safeView: {
+        flex: 1
+    },
+
+    cardStyles: {
+        padding: 10, 
+        paddingLeft: -40,
+        paddingBottom: -5,
+        marginBottom: 1,
+        width: 340,
+        height: 160,
+        marginLeft: 45,
+        marginTop: 35,
+        alignItems: 'center',
+        maxWidth: '100%'
     },
 
     bullyingImg: {
