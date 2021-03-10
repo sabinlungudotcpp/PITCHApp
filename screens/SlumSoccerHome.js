38b46ec072192345429b652c0b6c17a2cf6ec7c7
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-03-10 18:14:04 +0000

    Adding a ReactJS modal

--- screens/SlumSoccerHome.js
+++ screens/SlumSoccerHome.js
@@ -1,22 +1,40 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet, FlatList, ScrollView} from 'react-native';
+import {View, Text, Image, StyleSheet, FlatList, ScrollView, SafeAreaView} from 'react-native';
 import {Button} from 'react-native-elements';
 import Colors from '../constants/Colors';
 import Card from '../components/Card';
 
 const SlumSoccerHome = ({props, navigation}) => {
     return (
-        <View>
-            <Text style = {styles.introTxt}>Slum Soccer Home Screen</Text>
-        </View>
+        <SafeAreaView>
+            <ScrollView>
+                <Text style = {styles.introTxt}>Slum Soccer</Text>
+
+                <Image style = {styles.slumSoccerImg} source = {(require('../assets/Images/SlumSoccerImg.jpg'))} />
+
+                <Card style = {styles.cardStyle}>
+
+                </Card>
+
+                
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     introTxt: {
         textAlign: 'center',
-        fontSize: 22,
-        marginTop: 25
+        fontSize: 31,
+        marginTop: 31,
+        color: Colors.primaryColor
+    },
+
+    slumSoccerImg: {
+        width: 310,
+        height: 180,
+        marginLeft: 66,
+        marginTop: 30
     }
 });
 
