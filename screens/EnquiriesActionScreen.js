c33fb78318aec22c590aa08ea0e73bdcc82a124c
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-03-11 13:39:55 +0000

    Refactoring the Enquiries action screen to make it look good on Android

--- screens/EnquiriesActionScreen.js
+++ screens/EnquiriesActionScreen.js
@@ -144,7 +144,7 @@ const styles = StyleSheet.create({
     },
 
     enquiriesSubHeadingTxt: {
-        marginTop: -13
+        marginTop: Platform.OS === 'android' ? -18 : -13.5
     },
 
     allegationSubTxt: {
