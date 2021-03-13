102bb909e78b2936779f71dba796118a49226141
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-03-13 13:32:56 +0000

    Adding the content for Reporting Concerns

--- screens/ProvidingInformationScreen.js
+++ screens/ProvidingInformationScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {View, StyleSheet, FlatList, Image, Text, Platform, Modal, SafeAreaView, ScrollView} from 'react-native';
 import {Button} from 'react-native-elements';
 import Colors from '../constants/Colors';
@@ -7,21 +7,26 @@ import Card from '../components/Card';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const ProvidingInformationScreen = ({props, navigation, item}) => {
+    const [modalOpen, setModalOpen] = useState(false);
+
     return (
         <SafeAreaView style = {styles.defaultView}>
             <ScrollView>
                 <Text style = {styles.providingInfoHeader}>{ChildProtectionData.map(value => value.title_providing_information)}</Text>
 
+                <Modal animationType = {"slide"} visible = {modalOpen} transparent = {false}>
+                    <Text style = {styles.infoServiceTxt}>Information Services</Text>
+                </Modal>
+
                 <Image />
-
                 <Card style = {styles.cardStyle}>
-
+                    <FlatList data = {ChildProtectionData} keyExtractor = {(item) => item.id} renderItem = {({item}) => <Text>{item.providing_info_text}</Text>} />
                 </Card>
 
                 <View style = {styles.btnContainer}>
                     <Button style = {styles.backBtn} buttonStyle = {{backgroundColor: Colors.primaryColor, borderRadius: 200}} onPress = {() => navigation.navigate('ReportingConcernsScreen')} title = "Back" />
 
-                    <TouchableOpacity onPress = {() => {}} >
+                    <TouchableOpacity onPress = {() => setModalOpen(true)} >
                          <Button style = {styles.viewInfoBtn} buttonStyle = {{backgroundColor: Colors.primaryColor, borderRadius: 200}} title = "View Information" />
                     </TouchableOpacity>
                     
@@ -42,6 +47,19 @@ const styles = StyleSheet.create({
         color: Colors.primaryColor,
         fontSize: 29,
         marginTop: 60
+    },
+
+    cardStyle: { // Styles for the card
+        padding: 10, 
+        paddingLeft: -40,
+        paddingBottom: -5,
+        marginBottom: 1,
+        width: 340,
+        height: 160,
+        marginLeft: 48,
+        marginTop: 35,
+        alignItems: 'center',
+        maxWidth: '100%'
     },
 
     btnContainer: {
@@ -68,6 +86,17 @@ const styles = StyleSheet.create({
     viewInfoBtn: {
         width: 180,
         marginLeft: 30
+    },
+
+    modalBtn: {
+        marginTop: 300
+    },
+
+    infoServiceTxt: {
+        marginTop: 100,
+        textAlign: 'center',
+        color: Colors.primaryColor,
+        fontSize: 29
     }
 });
 
