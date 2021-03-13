f030dd298e3ae8462d2194b15057aec627a962be
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-03-13 11:07:21 +0000

    Adding the content for Reporting Concerns

--- screens/ProvidingInformationScreen.js
+++ screens/ProvidingInformationScreen.js
@@ -3,12 +3,25 @@ import {View, StyleSheet, FlatList, Image, Text, Platform, Modal, SafeAreaView, 
 import {Button} from 'react-native-elements';
 import Colors from '../constants/Colors';
 import ChildProtectionData from '../data/ChildProtectionData';
+import Card from '../components/Card';
 
 const ProvidingInformationScreen = ({props, navigation, item}) => {
     return (
         <SafeAreaView style = {styles.defaultView}>
             <ScrollView>
-                <Text>Providing Information Screen</Text>
+                <Text style = {styles.providingInfoHeader}>{ChildProtectionData.map(value => value.title_providing_information)}</Text>
+
+                <Image />
+
+                <Card style = {styles.cardStyle}>
+
+                </Card>
+
+                <View style = {styles.btnContainer}>
+                    <Button onPress = {() => {}} title = "Back" />
+                    <Button onPress = {() => {}} title = "View Information" />
+                </View>
+
             </ScrollView>
         </SafeAreaView>
     )
@@ -17,6 +30,13 @@ const ProvidingInformationScreen = ({props, navigation, item}) => {
 const styles = StyleSheet.create({
     defaultView: {
         flex: 1
+    },
+
+    providingInfoHeader: {
+        textAlign: 'center',
+        color: Colors.primaryColor,
+        fontSize: 29,
+        marginTop: 60
     }
 });
 
