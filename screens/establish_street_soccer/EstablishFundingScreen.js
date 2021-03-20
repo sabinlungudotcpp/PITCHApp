95bc172f92c7128e40b38924873f86c90bb7f9fa
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-03-20 10:35:57 +0000

    Adding code to the Establish Funding screen

--- screens/establish_street_soccer/EstablishFundingScreen.js
+++ screens/establish_street_soccer/EstablishFundingScreen.js
@@ -1,14 +1,26 @@
 import React, {useState} from "react";
-import {View, StyleSheet, Text, Platform, Modal, FlatList, ScrollView} from "react-native";
+import {View, StyleSheet, Text, Platform, Modal, FlatList, ScrollView, Image} from "react-native";
 import {Button} from "react-native-elements";
 import Card from "../../components/Card";
 import Colors from "../../constants/Colors";
 import EstablishStreetSoccerData from "../../data/EstablishStreetSoccerData";
 
 const EstablishFundingScreen = ({navigation, props}) => {
+
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const renderFundingBody = () => { // Renders the funding body of the screen
+        return (
+            <Image />
+        )
+    }
+
     return (
+     
         <ScrollView style = {styles.defaultView}>
             <Text style = {styles.fundingText}>{EstablishStreetSoccerData.map(title => title.funding_title)}</Text>
+
+            {renderFundingBody()}
         </ScrollView>
     )
 };
