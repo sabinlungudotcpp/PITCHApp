30d254e5e0a9224525e0a56badb6cdcfe2178058
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-03-20 10:40:42 +0000

    Adding code to the Establish Funding screen

--- screens/establish_street_soccer/EstablishFundingScreen.js
+++ screens/establish_street_soccer/EstablishFundingScreen.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import {View, StyleSheet, Text, Platform, Modal, FlatList, ScrollView, Image} from "react-native";
 import {Button} from "react-native-elements";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import Card from "../../components/Card";
 import Colors from "../../constants/Colors";
 import EstablishStreetSoccerData from "../../data/EstablishStreetSoccerData";
@@ -8,10 +9,25 @@ import EstablishStreetSoccerData from "../../data/EstablishStreetSoccerData";
 const EstablishFundingScreen = ({navigation, props}) => {
 
     const [modalOpen, setModalOpen] = useState(false);
+    const modalState = modalOpen;
+
+    const toggleModalHandler = () => { // Toggles the modal on
+        setModalOpen(!modalState);
+    };
+
+    const closeModalHandler = () => {
+        setModalOpen(modalState);
+    }
 
     const renderFundingBody = () => { // Renders the funding body of the screen
         return (
-            <Image />
+            <TouchableOpacity>
+                <Image style = {styles.fundingImg} source = {(require('../../assets/Images/ImgFunding.jpg'))} />
+
+                <Card>
+
+                </Card>
+            </TouchableOpacity>
         )
     }
 
@@ -19,7 +35,6 @@ const EstablishFundingScreen = ({navigation, props}) => {
      
         <ScrollView style = {styles.defaultView}>
             <Text style = {styles.fundingText}>{EstablishStreetSoccerData.map(title => title.funding_title)}</Text>
-
             {renderFundingBody()}
         </ScrollView>
     )
@@ -35,6 +50,13 @@ const styles = StyleSheet.create({
         fontSize: 29,
         color: Colors.secondaryColor,
         marginTop: 50
+    },
+
+    fundingImg: {
+        width: 250,
+        height: 150,
+        marginLeft: 88,
+        marginTop: 20
     }
 });
 
