d5860d37c680847a589f9947f2c7cf7edddd1d9a
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-03-20 17:42:31 +0000

    Adding code to the ME process in the modal

---
+++ screens/establish_street_soccer/EstablishFinalScreen.js
@@ -0,0 +1,16 @@
+import React, {useEffect, useRef} from 'react';
+import {View, StyleSheet, Platform, FlatList, ScrollView, Text} from "react-native";
+import {Button} from "react-native-elements";
+import Card from "../../components/Card";
+import Colors from "../../constants/Colors";
+import EstablishStreetSoccerData from "../../data/EstablishStreetSoccerData";
+
+const EstablishFinalScreen = ({navigation, props}) => {
+    return (
+        <ScrollView>
+            <Text></Text>
+        </ScrollView>
+    )
+};
+
+export default EstablishFinalScreen;
