849685cbbfb7cfd3db2d96dc0ad593441e75ba02
Sabin Constantin Lungu <sabin@Sabins-MBP.broadband>
2021-03-27 21:27:40 +0000

    Adding code to the proposition screen

--- screens/EduKickPropositionScreen.js
+++ screens/EduKickPropositionScreen.js
@@ -7,17 +7,19 @@ import Card from '../components/Card';
 
 const EduKickPropositionScreen = ({props, navigation}) => {
     return (
+
         <ScrollView>
             <Text style = {styles.propositionTitle}>{EduKickData.map(title => title.edu_title_proposition)}</Text>
 
             <Image style = {styles.propositionImg} source = {(require('../assets/Images/ImageProposition.jpg'))} />
 
             <Card style = {styles.cardStyle}>
-                <FlatList />
+                <FlatList data = {EduKickData} keyExtractor = {(item) => item.id} renderItem = {({item}) => <Text></Text>} />
             </Card>
 
             <View style = {styles.btnContainer}>
-
+                <Button title = "Back" />
+                <Button title = "View More" />
             </View>
 
 
